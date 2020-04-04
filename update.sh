c6f76de9226946ac5a3669a3e58b20be85f21fb0
jikkyfu <jikkyfu@gmail.com>
2020-04-04 22:56:50 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -6,8 +6,9 @@ rm anti-ad-for-smartdns.conf
 wget https://gitee.com/privacy-protection-tools/anti-ad/raw/master/anti-ad-for-smartdns.conf && echo -n "  start update anti-ad-for-smartdns.conf " >> 1.log 
 rm make-gfwlist.sh && rm gfwlist.conf
 wget https://raw.githubusercontent.com/jikkyfu/smartdns/master/make-gfwlist.sh
-sh make-gfwlist.sh -d oversea -o gfwlist.conf && echo " +++++  update gfwlist.conf "  >> 1.log
+sh make-gfwlist.sh -d oversea -o gfwlist.conf && echo -n " +++++  update gfwlist.conf "  >> 1.log
 wget https://raw.githubusercontent.com/jikkyfu/smartdns/master/blacklist-ip.conf
 wget https://raw.githubusercontent.com/jikkyfu/smartdns/master/custom.conf
 sed -ie '/smartdns/d' /var/spool/cron/crontabs/root
+#sh ./merge.sh  &&  echo " +++++  update merge-for-smartdns.conf "  >> 1.log
 echo "0 4 * * * bash /etc/smartdns/update.sh" >> /var/spool/cron/crontabs/root
