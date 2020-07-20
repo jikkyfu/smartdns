325311b8fdae7962aa660cb2d7c67d526c0c8ff7
jikkyfu <jikkyfu@gmail.com>
2020-07-20 20:07:47 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,18 +1,19 @@
 cd /etc/smartdns
 date "+%Y-%m-%d %H:%M:%S" > 1.log
-#rm address.conf && 
-rm blacklist-ip.conf && rm custom.conf
-rm anti-ad-for-smartdns.conf 
-rm neohosts-for-smartdns.conf
-rm merge-for-smartdns.conf
+
+rm /etc/smartdns/* -f
+wget https://raw.githubusercontent.com/jikkyfu/smartdns/master/update.sh
+
 wget https://gitee.com/privacy-protection-tools/anti-ad/raw/master/anti-ad-for-smartdns.conf && echo -n "  start update anti-ad-for-smartdns.conf " >> 1.log 
-rm make-gfwlist.sh && rm gfwlist.conf
+
 wget https://raw.githubusercontent.com/jikkyfu/smartdns/master/make-gfwlist.sh
 sh make-gfwlist.sh -d oversea -o gfwlist.conf && echo -n " +++++  update gfwlist.conf "  >> 1.log
+
 wget https://raw.githubusercontent.com/jikkyfu/smartdns/master/blacklist-ip.conf
 wget https://raw.githubusercontent.com/jikkyfu/smartdns/master/custom.conf
 wget https://raw.githubusercontent.com/jikkyfu/smartdns/master/merge.sh
-wget https://raw.githubusercontent.com/jikkyfu/smartdns/master/go.sh
-sed -ie '/smartdns/d' /var/spool/cron/crontabs/root
+
 sh ./merge.sh  &&  echo " +++++  update merge-for-smartdns.conf "  >> 1.log
+
+sed -ie '/smartdns/d' /var/spool/cron/crontabs/root
 echo "0 4 * * * sh /etc/smartdns/update.sh" >> /var/spool/cron/crontabs/root
