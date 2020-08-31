23fac4247951b7de188190599ee60422ff48fa96
jikkyfu <jikkyfu@gmail.com>
2020-08-31 09:24:34 +0800

    Update make-gfwlist.sh

--- tmp/make-gfwlist.sh
+++ tmp/make-gfwlist.sh
@@ -256,11 +256,11 @@ process(){
     # Convert domains into smartdns rules
         if [ $WITH_IPSET -eq 1 ]; then
             _green 'Ipset rules included.'
-            sort -u $DOMAIN_FILE | $SED_ERES 's#(.+)#nameserver /.\1/'$DNS_IP' '$DNS_PORT'\
+            sort -u $DOMAIN_FILE | $SED_ERES 's#(.+)#nameserver /\1/'$DNS_IP' '$DNS_PORT'\
 ipset=/\1/'$IPSET_NAME'#g' > $CONF_TMP_FILE
         else
             _green 'Ipset rules not included.'
-            sort -u $DOMAIN_FILE | $SED_ERES 's#(.+)#nameserver /.\1/'$DNS_IP' '$DNS_PORT'#g' > $CONF_TMP_FILE
+            sort -u $DOMAIN_FILE | $SED_ERES 's#(.+)#nameserver /\1/'$DNS_IP' '$DNS_PORT'#g' > $CONF_TMP_FILE
         fi
 
         # Generate output file
