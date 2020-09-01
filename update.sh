0a2d3c504057243b663ae18230ad9d0decc23f84
jikkyfu <jikkyfu@gmail.com>
2020-09-01 14:09:49 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,9 +1,12 @@
 rm latest
 wget https://github.com/pymumu/smartdns/releases/latest
-link=`cat latest |grep -m1  /pymumu/smartdns/commit/ |awk -F 'commit/' '{print $2}' |awk  -F '"' '{print $1}'`
-version=`cat latest |grep -m1  x86_64-openwrt-all.ipk |awk -F 'smartdns.' '{print $3}' |awk  -F '.x86' '{print $1}'`
-echo $link
+url=`cat latest |grep -m1  /pymumu/smartdns/commit/ |awk -F 'commit/' '{print $2}' |awk  -F '"' '{print $1}'`
+version=`cat latest |grep -m1  x86_64-openwrt-all.ipk |awk -F 'smartdns.' '{print $3}' |awk  -F '.x86' '{print $1}'|awk  -F '-' '{print $1}'`
+release=`cat latest |grep -m1  x86_64-openwrt-all.ipk |awk -F 'smartdns.' '{print $3}' |awk  -F '.x86' '{print $1}'|awk  -F '-' '{print $2}'`
+echo $url
 echo $version
+echo $release
 
 sed -ri "s/(PKG_VERSION:=)[^=]*/\1$version/" Makefile
-sed -ri "s/(PKG_SOURCE_VERSION:=)[^=]*/\1$link/" Makefile
+sed -ri "s/(PKG_RELEASE:=)[^=]*/\1$release/" Makefile
+sed -ri "s/(PKG_SOURCE_VERSION:=)[^=]*/\1$url/" Makefile
